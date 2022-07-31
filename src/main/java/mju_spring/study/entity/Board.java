706bebd22c7e448551645662fecf595f9f14d954
chrkb1569 <chrkb1569@naver.com>
2022-07-31 15:49:24 +0900

    feat : Add Constructor for DTO

--- src/main/java/mju_spring/study/entity/Board.java
+++ src/main/java/mju_spring/study/entity/Board.java
@@ -4,14 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Board {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,4 +24,9 @@ public class Board {
 
     private String writer;
 
+    public Board(String title, String content, String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
 }
